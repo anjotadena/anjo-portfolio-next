87bc153065edea55fbef5ff243e43c4497778410
Anjo Tadena <tadena.anjo@gmail.com>
2024-12-10 20:02:40 +0800

    chore: Update site.webmanifest with additional icons and metadata

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -7,11 +7,13 @@ const Resume = () => {
       title: "Software Engineer",
       company: "Nelnet Philippines Inc. - Manila",
       description:
-        "Slashnode works with SMEs and startups in Australia and abroad to execute web projects using best-in-class processes, tools, and technology stacks, with a focus on Laravel and NestJS frameworks. Slashnode offers a range of services and solutions, including:",
+        "Nelnet Philippines Inc., is a software services subsidiary of Nelnet, Inc., a diversified company focusing on education-related products, services, and student loan asset management. NPI supports IT development teams across Nelnet Business Services, collaborating on applications for the education, nonprofit, and religious sectors. The company offers opportunities in various programming languages and technologies.",
       points: [
-        "Fractional CTO services: Acting as the part-time CTO for startups and SMEs. Advising on technology choices, assisting in the hiring process, and optimizing development processes and procedures (including DevOps and deployment automation).",
-        "AppSec/Cybersecurity reviews of Laravel and NestJS codebases.",
-        "Technical due diligence: Reviewing digital datarooms and code repositories for SMEs during the due diligence phase of company acquisitions.",
+        "Design and implement scalable, secure, and maintainable applications using ASP.NET Core, Angular, and modern technologies.",
+        "Architect, develop, and optimize microservices-based solutions to improve scalability, performance, and reliability.",
+        "Leverage AWS and Azure for building cloud-native solutions, including serverless architectures and containerized services.",
+        "Guide and support junior engineers through code reviews and technical mentorship, fostering skill development within the team.",
+        "Drive process optimizations, CI/CD pipeline enhancements, and the adoption of emerging technologies to improve team efficiency and product quality."
       ],
     },
     {
@@ -19,11 +21,13 @@ const Resume = () => {
       title: "Senior Software Engineer",
       company: "Prosource BPO - Manila",
       description:
-        "Spearheaded technical operations and development strategies for a mid-sized technology company.",
+        "ProSource BPO is a business process outsourcing company based in Makati, Philippines, with additional offices in Hong Kong. It specializes in building and managing distributed teams for global clients, focusing on highly technical roles and expertise in technologies such as .NET, AWS, Angular, Java, Salesforce, and more.",
       points: [
-        "Led a team of 30+ engineers to deliver multiple enterprise-grade SaaS products.",
-        "Optimized CI/CD pipelines, reducing deployment time by 50%.",
-        "Oversaw company-wide migration to a microservices architecture, improving system scalability by 300%.",
+        "Enhanced and maintained an existing client product using C#, Angular, Azure, VueJS, and Ionic.",
+        "Designed and implemented new features to meet client requirements while ensuring product quality and reliability.",
+        "Reported directly to the Product Owner, providing updates and aligning on project goals and deliverables.",
+        "Collaborated with cross-functional teams to ensure smooth project execution and timely delivery of tasks.",
+        "Leveraged Azure for cloud-based solutions and utilized modern frameworks like VueJS and Ionic for enhanced user experience."
       ],
     },
     {
@@ -31,11 +35,13 @@ const Resume = () => {
       title: "Software Engineer",
       company: "Arcanys/XLR8 Hub Inc. - Cebu",
       description:
-        "Developed and maintained web applications for various industries, contributing to the full development lifecycle.",
+        "Arcanys is a Swiss software development outsourcing company founded in 2010 by entrepreneurs Alan Debonneville and Frederic Joye. Based in the Philippines, Arcanys specializes in building dedicated teams of skilled developers for tech-enabled businesses, offering services such as user interface design, business analysis, prototyping, software testing, and application development. The company caters to various sectors, including fintech, media, and healthcare, emphasizing a people-centric approach to foster a collaborative work environment and ensure high employee satisfaction and retention rates.",
       points: [
-        "Designed and implemented RESTful APIs using Node.js and Express.",
-        "Built responsive front-end interfaces with React and Angular.",
-        "Collaborated with cross-functional teams to meet client requirements on time.",
+        "Designed, developed, and maintained client-specific systems utilizing ReactJS for front-end development and C# for back-end services.",
+        "Engaged directly with the client to gather requirements, provide progress updates, and ensure the delivered solutions met their expectations.",
+        "Implemented best practices in coding standards, conducted code reviews, and performed thorough testing to ensure high-quality deliverables.",
+        "Collaborated with cross-functional teams, including designers, testers, and project managers, to ensure seamless integration and functionality of the system.",
+        "Created and maintained comprehensive documentation of the system architecture, codebase, and user guides to facilitate knowledge sharing and future maintenance."
       ],
     },
     {
@@ -43,11 +49,13 @@ const Resume = () => {
       title: "Software Engineer",
       company: "Sprobe Inc. - Cebu",
       description:
-        "Developed and maintained web applications for various industries, contributing to the full development lifecycle.",
+        "Sprobe Inc. is a Japan-affiliated company located in Cebu Business Park, Philippines, specializing in both IT Outsourcing (ITO) and Business Process Outsourcing (BPO) services. Established in 2012, Sprobe delivers software development and systems integration projects for clients primarily from Japan and Singapore, encompassing web and mobile applications, UI/UX design, and creative services. The company emphasizes core values such as Self-Discipline, Professionalism, Respect, Openness, Balance, and Excellence, fostering a family-like, innovative, and energetic work environment.",
       points: [
-        "Designed and implemented RESTful APIs using Node.js and Express.",
-        "Built responsive front-end interfaces with React and Angular.",
-        "Collaborated with cross-functional teams to meet client requirements on time.",
+        "Designed, developed, and maintained client-specific systems utilizing ReactJS for front-end development and C# for back-end services.",
+        "Engaged directly with clients to gather requirements, provide progress updates, and ensure the delivered solutions met their expectations.",
+        "Implemented best practices in coding standards, conducted code reviews, and performed thorough testing to ensure high-quality deliverables.",
+        "Collaborated with cross-functional teams, including designers, testers, and project managers, to ensure seamless integration and functionality of the system.",
+        "Created and maintained comprehensive documentation of the system architecture, codebase, and user guides to facilitate knowledge sharing and future maintenance."
       ],
     },
     {
@@ -55,11 +63,14 @@ const Resume = () => {
       title: "Software Engineer",
       company: "Code Republic/Ark Creative Studio - Cebu",
       description:
-        "Developed and maintained web applications for various industries, contributing to the full development lifecycle.",
+        "Code Republiq is a technology division of RGC, a full-service digital agency based in Cebu, Philippines. The company specializes in providing technology and engineering services, including web development, mobile app development, UX/UI design, e-commerce solutions, and online business strategies. Code Republiq collaborates with other RGC divisions to deliver comprehensive digital transformation and creative solutions. It emphasizes innovative and efficient approaches to meet diverse client needs.",
       points: [
-        "Designed and implemented RESTful APIs using Node.js and Express.",
-        "Built responsive front-end interfaces with React and Angular.",
-        "Collaborated with cross-functional teams to meet client requirements on time.",
+        "Designed, developed, and maintained client-specific systems using ReactJS, NodeJS, Laravel, Angular, and serverless technologies.",
+        "Built scalable and serverless applications, ensuring seamless integration with backend services and optimal system performance.",
+        "Worked directly with clients to gather requirements, provide progress updates, and deliver tailored solutions to meet their business objectives.",
+        "Ensured high-quality deliverables through best coding practices, regular code reviews, and thorough testing procedures.",
+        "Collaborated with designers, testers, and project managers to ensure the timely delivery of robust and user-friendly solutions.",
+        "Created and maintained comprehensive technical documentation, including system architecture, API references, and user guides, to facilitate seamless knowledge sharing and system maintenance."
       ],
     },
     {
@@ -67,11 +78,14 @@ const Resume = () => {
       title: "Software Engineer",
       company: "Central Mindanao University - Mindanao",
       description:
-        "Developed and maintained web applications for various industries, contributing to the full development lifecycle.",
+        "Central Mindanao University (CMU) is a premier state university located in the Philippines, renowned for its excellence in instruction, research, and extension services. CMU focuses on providing high-quality education and fostering innovation across various disciplines. Its IT initiatives include developing and maintaining digital systems to support academic and administrative functions.",
       points: [
-        "Designed and implemented RESTful APIs using Node.js and Express.",
-        "Built responsive front-end interfaces with React and Angular.",
-        "Collaborated with cross-functional teams to meet client requirements on time.",
+        "Designed and developed the Library System for Research and Thesis Management from scratch using VueJS for the front-end and Laravel for the back-end.",
+        "Implemented features allowing students and graduates to upload, categorize, and search for research and thesis entries, ensuring ease of use and accessibility.",
+        "Integrated logic to identify and flag potential duplicate submissions, promoting originality and integrity in academic research.",
+        "Designed and optimized the database schema to store and manage large volumes of research data efficiently.",
+        "Engaged with university administrators, librarians, and students to gather requirements and ensure the system met their specific needs.",
+        "Deployed the system, provided technical support, and trained end-users to ensure smooth adoption and usage across the university."
       ],
     },
   ];
@@ -88,17 +102,17 @@ const Resume = () => {
         </p>
 
         {/* Timeline as a List */}
-        <ol className="relative border-s border-gray-500 dark:border-gray-700">
+        <ol className="relative border-s border-gray-300 dark:border-gray-600">
           {timelineData.map((item, index) => (
             <li key={index} className="mb-10 ml-6">
               {/* Dot */}
-              <span className="absolute w-3 h-3 bg-gray-700 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></span>
+              <span className="absolute w-3 h-3 bg-gray-400 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></span>
 
               {/* Date */}
-              <p className="text-sm text-gray-900">{item.date}</p>
+              <p className="text-sm text-gray-500">{item.date}</p>
 
               {/* Title and Company */}
-              <h3 className="text-xl font-semibold text-blue-700">{item.title}</h3>
+              <h3 className="text-xl font-semibold text-blue-800">{item.title}</h3>
               <h4 className="text-md font-light text-blue-500">
                 {item.company}
               </h4>
@@ -107,10 +121,11 @@ const Resume = () => {
               <p className="mt-2 text-gray-900">{item.description}</p>
 
               {/* Bullet Points */}
+              <h5 className="mt-4 font-semibold">Key Responsibilities:</h5>
               <ul className="mt-4 space-y-2 text-gray-900 list-disc list-inside">
                 {item.points.map((point, i) => (
                   <li key={i} className="flex items-start">
-                    <span className="text-green-400 mt-1 mr-2">✔</span>
+                    <span className="text-blue-500 mt-1 mr-2">✔</span>
                     {point}
                   </li>
                 ))}
